2c0b2386b716bebda5a1b2f7fa45f8f85e73be98
Sebastiano Poggi <poggos@gmail.com>
2017-06-12 10:47:16 +0100

    Simplify ViewPagerAdapter logic, remove state class

--- library/src/main/java/com/novoda/viewpageradapter/ViewPagerAdapter.java
+++ library/src/main/java/com/novoda/viewpageradapter/ViewPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.novoda.viewpageradapter;
 
+import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
@@ -8,29 +9,49 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public abstract class ViewPagerAdapter<V extends View> extends PagerAdapter {
 
-    private final Map<V, Integer> instantiatedViews = new HashMap<>();
-    private final ViewIdGenerator viewIdGenerator = new ViewIdGenerator();
-
-    private ViewPagerAdapterState viewPagerAdapterState = ViewPagerAdapterState.newInstance();
+    private static final String STATE_KEY_VIEW_STATES = "states";
+    private final Map<V, Integer> instantiatedViews = new HashMap<>();               // TODO errrrrrrrrrrrrr
+    private SparseArray<Parcelable> viewStates = new SparseArray<>();
+    private final Set<Integer> knownIds = new HashSet<>();
 
     @Override
     public V instantiateItem(ViewGroup container, int position) {
         V view = createView(container, position);
-        SparseArray<Parcelable> viewState = viewPagerAdapterState.getViewState(position);
 
-        int restoredId = viewPagerAdapterState.getId(position);
-        view.setId(restoredId == View.NO_ID ? viewIdGenerator.generateViewId() : restoredId);
+        if (expectsToSaveStateAt(position)) {
+            assertViewStateCanBeSavedFor(view, position);
+        }
 
-        bindView(view, position, viewState);
+        bindView(view, position, viewStates);
         instantiatedViews.put(view, position);
         container.addView(view);
+        saveViewIdIfSet(view);
 
         // key with which to associate this view
         return view;
+    }
+
+    protected boolean expectsToSaveStateAt(int position) {
+        return false;
+    }
+
+    private void assertViewStateCanBeSavedFor(V view, int position) {
+        if (view.getId() == View.NO_ID || knownIds.contains(view.getId())) {
+            throw new IllegalArgumentException("The view created for position " + position + " must have a unique view ID");
+        }
+    }
+
+    private void saveViewIdIfSet(V view) {
+        if (view.getId() == View.NO_ID) {
+            return;
+        }
+        knownIds.add(view.getId());
     }
 
     /**
@@ -47,14 +68,14 @@ public abstract class ViewPagerAdapter<V extends View> extends PagerAdapter {
     /**
      * Bind the view to the item at the given position with view state.
      *
-     * @param view      the view to bind
-     * @param position  the position of the data set that is to be represented by this view
-     * @param viewState the state of the view
+     * @param view       the view to bind
+     * @param position   the position of the data set that is to be represented by this view
+     * @param viewStates all the saved view states
      */
-    protected void bindView(V view, int position, @Nullable SparseArray<Parcelable> viewState) {
+    protected final void bindView(V view, int position, SparseArray<Parcelable> viewStates) {
         bindView(view, position);
-        if (viewState != null) {
-            view.restoreHierarchyState(viewState);
+        if (expectsToSaveStateAt(position)) {
+            view.restoreHierarchyState(viewStates);
         }
     }
 
@@ -72,48 +93,49 @@ public abstract class ViewPagerAdapter<V extends View> extends PagerAdapter {
         super.notifyDataSetChanged();
         for (Map.Entry<V, Integer> entry : instantiatedViews.entrySet()) {
             int position = entry.getValue();
-            SparseArray<Parcelable> viewState = viewPagerAdapterState.getViewState(position);
-            bindView(entry.getKey(), position, viewState);
+            bindView(entry.getKey(), position, viewStates);
         }
     }
 
-    @SuppressWarnings("unchecked")
-    // `key` is the object we return in `instantiateItem(ViewGroup container, int position)`
+    @SuppressWarnings("unchecked")    // `key` is the object we return in `instantiateItem(ViewGroup container, int position)`
     @Override
     public void destroyItem(ViewGroup container, int position, Object key) {
         V view = (V) key;
-        saveViewState(position, view);
+        saveViewState(view);
         container.removeView(view);
         instantiatedViews.remove(view);
+
+        if (view.getId() != View.NO_ID) {
+            knownIds.remove(view.getId());
+        }
     }
 
-    private void saveViewState(int position, V view) {
-        SparseArray<Parcelable> viewState = new SparseArray<>();
-        view.saveHierarchyState(viewState);
-        viewPagerAdapterState.put(view.getId(), position, viewState);
+    private void saveViewState(V view) {
+        view.saveHierarchyState(viewStates);
     }
 
     @Override
     public Parcelable saveState() {
-        for (Map.Entry<V, Integer> entry : instantiatedViews.entrySet()) {
-            int position = entry.getValue();
-            V view = entry.getKey();
-            saveViewState(position, view);
+        Bundle state = null;
+        if (viewStates.size() > 0) {
+            state = new Bundle();
+            state.putSparseParcelableArray(STATE_KEY_VIEW_STATES, viewStates);
         }
-        return viewPagerAdapterState;
+        return state;
     }
 
     @Override
-    public void restoreState(Parcelable state, ClassLoader loader) {
-        if (state instanceof ViewPagerAdapterState) {
-            this.viewPagerAdapterState = ((ViewPagerAdapterState) state);
-        } else {
-            super.restoreState(state, loader);
+    public void restoreState(@Nullable Parcelable state, @Nullable ClassLoader loader) {
+        super.restoreState(state, loader);
+        if (state instanceof Bundle) {
+            Bundle bundle = (Bundle) state;
+            bundle.setClassLoader(loader);
+            viewStates = bundle.getSparseParcelableArray(STATE_KEY_VIEW_STATES);
         }
     }
 
     @Override
     public boolean isViewFromObject(View view, Object key) {
-        return view.equals(key);
+        return view.getId() == ((View) key).getId();
     }
 }
